4df517ab6c75c7c32764cfcdbd380071d89102c2
rittik sheikh <rittik11500@gamil.com>
2023-04-20 05:13:15 +0600

    all server site route has done

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 const cors = require('cors');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('server is running successfully');
 });
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter( n => n.category_id === id)
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+})
+
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
